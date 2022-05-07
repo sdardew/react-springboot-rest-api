25ee6756286078bcb6111dbcb3b376c9d1f0a4c5
sdardew <sdardew@gmail.com>
2022-05-07 13:32:57 +0900

    Create server

---
+++ coffee-order-server/src/main/java/com/sdardew/coffeeorderserver/CoffeeOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.sdardew.coffeeorderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoffeeOrderServerApplication.class, args);
+  }
+
+}
---
+++ coffee-order-server/src/test/java/com/sdardew/coffeeorderserver/CoffeeOrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdardew.coffeeorderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
