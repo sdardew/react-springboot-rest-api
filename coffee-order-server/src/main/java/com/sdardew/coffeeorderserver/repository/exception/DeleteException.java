050f0e68218578885185c1282119f411757389cd
sdardew <sdardew@gmail.com>
2022-05-08 02:11:06 +0900

    chore: DeleteException

---
+++ coffee-order-server/src/main/java/com/sdardew/coffeeorderserver/repository/exception/DeleteException.java
@@ -0,0 +1,7 @@
+package com.sdardew.coffeeorderserver.repository.exception;
+
+public class DeleteException extends RuntimeException {
+  public DeleteException(String msg) {
+    super(msg);
+  }
+}
