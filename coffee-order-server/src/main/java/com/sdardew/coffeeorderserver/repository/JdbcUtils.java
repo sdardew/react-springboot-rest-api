400c5c566d8c4a617891ebc12f3610d3cbd64699
sdardew <sdardew@gmail.com>
2022-05-07 19:45:10 +0900

    feat: JdbcUtils
    Jdbc에서 공통적으로 사용하는 메서드들을 관리

---
+++ coffee-order-server/src/main/java/com/sdardew/coffeeorderserver/repository/JdbcUtils.java
@@ -0,0 +1,13 @@
+package com.sdardew.coffeeorderserver.repository;
+
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class JdbcUtils {
+  public static UUID toUUID(byte[] bytes) {
+    var byteBuffer = ByteBuffer.wrap(bytes);
+    return new UUID(byteBuffer.getLong(), byteBuffer.getLong());
+  }
+}
